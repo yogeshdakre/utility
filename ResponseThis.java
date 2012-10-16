34413e6628bf7be0083c9abf5bb0f02553f8b95a
yogeshd <yogeshd@cybage.com>
2012-10-16 10:37:20 +0530

    data

---
+++ ResponseThis.java
@@ -0,0 +1,1 @@
+public class ResponseThis {private String response;public String getResponse() {return response;}public void setResponse(String response){this.response=response;}private String tap_id;public String getTap_id() {return tap_id;}public void setTap_id(String tap_id){this.tap_id=tap_id;}}
---
+++ TestThis.java
@@ -0,0 +1,1 @@
+public class TestThis {private String user_id;public String getUser_id() {return user_id;}public void setUser_id(String user_id){this.user_id=user_id;}private String device_id;public String getDevice_id() {return device_id;}public void setDevice_id(String device_id){this.device_id=device_id;}}
